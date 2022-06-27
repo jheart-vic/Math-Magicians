40cb936e11e331e8734f6de15e6845e9d9ed032b
jheart-vic <adebowalevictorjheart.com>
2022-06-27 12:52:59 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, React!</h1>
-      <h1>We Are Getting Started</h1>
+      <h1>We Are Getting Started Now!</h1>
 
     </div>
   );
