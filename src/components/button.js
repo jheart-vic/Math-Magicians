df2517491def2db6d01836ac17e4a8b5dde0acd7
jheart-vic <adebowalevictorjheart.com>
2022-07-06 15:16:11 +0100

    Add test for user interactions

--- src/components/button.js
+++ src/components/button.js
@@ -6,33 +6,33 @@ const Button = (props) => {
   return (
     <div className="btns-container">
       <div className="btn-container">
-        <button type="button" onClick={() => buttonClick('AC')}>AC</button>
-        <button type="button" onClick={() => buttonClick('+/-')}>+/-</button>
-        <button type="button" onClick={() => buttonClick('%')}>%</button>
-        <button type="button" onClick={() => buttonClick('÷')} className="orange">&#247;</button>
+        <button type="button" data-testid="clear" onClick={() => buttonClick('AC')}>AC</button>
+        <button type="button" data-testid="plus-minus" onClick={() => buttonClick('+/-')}>+/-</button>
+        <button type="button" data-testid="mol" onClick={() => buttonClick('%')}>%</button>
+        <button type="button" data-testid="divide" onClick={() => buttonClick('÷')} className="orange">&#247;</button>
       </div>
       <div className="btn-container">
-        <button type="button" onClick={() => buttonClick('7')}>7</button>
-        <button type="button" onClick={() => buttonClick('8')}>8</button>
-        <button type="button" onClick={() => buttonClick('9')}>9</button>
-        <button type="button" onClick={() => buttonClick('x')} className="orange">x</button>
+        <button type="button" data-testid="seven" onClick={() => buttonClick('7')}>7</button>
+        <button type="button" data-testid="eight" onClick={() => buttonClick('8')}>8</button>
+        <button type="button" data-testid="nine" onClick={() => buttonClick('9')}>9</button>
+        <button type="button" data-testid="times" onClick={() => buttonClick('x')} className="orange">x</button>
       </div>
       <div className="btn-container">
-        <button type="button" onClick={() => buttonClick('4')}>4</button>
-        <button type="button" onClick={() => buttonClick('5')}>5</button>
-        <button type="button" onClick={() => buttonClick('6')}>6</button>
-        <button type="button" onClick={() => buttonClick('-')} className="orange">-</button>
+        <button type="button" data-testid="four" onClick={() => buttonClick('4')}>4</button>
+        <button type="button" data-testid="five" onClick={() => buttonClick('5')}>5</button>
+        <button type="button" data-testid="six" onClick={() => buttonClick('6')}>6</button>
+        <button type="button" data-testid="minus" onClick={() => buttonClick('-')} className="orange">-</button>
       </div>
       <div className="btn-container">
-        <button type="button" onClick={() => buttonClick('1')}>1</button>
-        <button type="button" onClick={() => buttonClick('2')}>2</button>
-        <button type="button" onClick={() => buttonClick('3')}>3</button>
-        <button type="button" onClick={() => buttonClick('+')} className="orange">+</button>
+        <button type="button" data-testid="one" onClick={() => buttonClick('1')}>1</button>
+        <button type="button" data-testid="two" onClick={() => buttonClick('2')}>2</button>
+        <button type="button" data-testid="three" onClick={() => buttonClick('3')}>3</button>
+        <button type="button" data-testid="plus" onClick={() => buttonClick('+')} className="orange">+</button>
       </div>
       <div className="btn-container">
-        <button type="button" onClick={() => buttonClick('0')} className="bigBtn">0</button>
-        <button type="button" onClick={() => buttonClick('.')} className="Dot">.</button>
-        <button type="button" onClick={() => buttonClick('=')} className="orange">=</button>
+        <button type="button" data-testid="zero" onClick={() => buttonClick('0')} className="bigBtn">0</button>
+        <button type="button" data-testid="dot" onClick={() => buttonClick('.')} className="Dot">.</button>
+        <button type="button" data-testid="equalSign" onClick={() => buttonClick('=')} className="orange">=</button>
       </div>
     </div>
   );
---
+++ src/testing/button.test.js
@@ -0,0 +1,23 @@
+import { screen,fireEvent, render} from '@testing-library/react';
+// import Button from '../components/button';
+// import InputResult from '../components/inputDisplay'
+import Calculator from '../components/Calculator'
+
+test('Testing onClick events',  () => {
+  render(<Calculator />)
+  expect(screen.getByTestId("displayInput")).toHaveTextContent("0");
+  
+})
+
+test('Testing onClick events',  () => {
+  render(<Calculator />)
+  const fireOne = screen.getByTestId("one");
+  fireEvent.click(fireOne)
+  expect(screen.getByTestId("displayInput")).toHaveTextContent("1");
+})
+test('Testing onClick events',  () => {
+  render(<Calculator />)
+  const fireOne = screen.getByTestId("plus");
+  fireEvent.click(fireOne)
+  expect(screen.getByTestId("displayInput")).toHaveTextContent("+");
+})
